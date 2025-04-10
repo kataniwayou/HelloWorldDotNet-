6e59e42f557882d9886c1d244c6632084218d2db
kataniwayou <kataniwayou@gmail.com>
2025-04-10 14:25:53 +0300

    Add seconds counter to demo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿﻿﻿// See https://aka.ms/new-console-template for more information
+﻿﻿﻿﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, liran World!");
 Console.WriteLine("my demo is working");
+
+int counter = 60;
+while (counter >= 0)
+{
+    Console.WriteLine($"Seconds remaining: {counter}");
+    System.Threading.Thread.Sleep(1000); // Wait 1 second
+    counter--;
+}
+
+Console.WriteLine("Press any key to terminate.");
+Console.ReadKey();
