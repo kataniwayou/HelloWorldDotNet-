a050d23e13a4739528ba7e36f9ea09ee14c373d3
kataniwayou <kataniwayou@gmail.com>
2025-04-10 10:06:58 +0300

    Run project and verify output

--- obj/Debug/net9.0/HelloWorldDotNet.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloWorldDotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldDotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b679225b1bd6b4e6fc2fc56f2ece91d8a652e69")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldDotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldDotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
